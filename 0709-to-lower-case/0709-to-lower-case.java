c69b7ce5ebf93b88d60f39062a85d899c9a612fe
Aman Panwar <amanpanwar.business@gmail.com>
2026-07-12 21:03:05 +0530

    Time: 0 ms (100%), Space: 42.7 MB (83.63%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char[] ch = s.toCharArray(); 
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 65 && ch[i] <= 90) { // A = 65 , Z = 90 , a = 65+32
+                ch[i] = (char)(ch[i] + 32); // Explicit type Casting.
+            }
+        }
+        return new String(ch); 
+    }
+}
