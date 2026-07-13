9359f8ef223c0a0bf86dacd1d940d94848bcf67f
Aman Panwar <amanpanwar.business@gmail.com>
2026-07-13 22:49:41 +0530

    Time: 6 ms (21.94%), Space: 46 MB (53.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -15,3 +15,20 @@ class Solution {
         
     }
 }
+//  optimize code : ---------------------------------------------------------------
+// class Solution {
+//     public boolean isPalindrome(int n) {
+//         int original =n;
+//         int rev = 0;
+
+//         while(n > 0) {
+//             int digit = n % 10;   // last digit
+//             rev = rev * 10 + digit;
+//             n = n / 10;
+//         }
+//         if(rev == original )
+//             return true;
+//         else
+//             return false;
+//     }
+// }
